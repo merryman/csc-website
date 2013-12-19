b328f0e52135911d9a0634ae92d1ed36109040f8
Robin Schreiber <robin.schreiber@me.com>
2013-12-19 15:04:58 -0800

    add creation of status updates

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,3 +1,3 @@
 class WelcomeController < ApplicationController
-  @status_updates = []
+  
 end
--- app/models/status_update.rb
+++ app/models/status_update.rb
@@ -1,2 +1,7 @@
 class StatusUpdate < ActiveRecord::Base
+  
+  def self.save_status_update(content)
+    create!(:date => Date.today, :content => content)
+  end
+  
 end
